99c3022d4fe090fed85adb540900983f10cb44c3
Tanishk Malhotra <80147670+tanishkmalhotrx@users.noreply.github.com>
2023-06-06 14:32:22 +0530

    final commit: sending the screenshot in main

--- emailing.py
+++ emailing.py
@@ -1,4 +1,26 @@
-from smtplib import SMTP
+import smtplib
+import imghdr
+from email.message import EmailMessage
 
-def send_email():
-    print("Email sent!")
+password = "ydwkxrilrjmluedd"
+sender = "tanishkmalhotra190804@gmail.com"
+receiver = "tanishkvmalhotra@gmail.com"
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "Person found!"
+    email_message.set_content("Hey, a person was spot on the camera.")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content), filename="image")
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(sender, password)
+    gmail.sendmail(sender, receiver, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import time
 import glob
+import os
+from threading import Thread
 from emailing import send_email
 
 video = cv2.VideoCapture(0)
@@ -9,6 +11,11 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+def clean_dir():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 while True:
     status = 0
@@ -40,7 +47,12 @@ while True:
     status_list.append(status)
     status_list = status_list[-2:]
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(img_with_obj, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_dir)
+        clean_thread.daemon = True
+
+        email_thread.start()
 
     cv2.imshow("video", frame)
     key = cv2.waitKey(1)
@@ -49,3 +61,4 @@ while True:
         break
 
 video.release()
+clean_thread.start()
